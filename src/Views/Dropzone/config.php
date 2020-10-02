1d0be25022a79d947535cd69d16b3168748b69d0
MGatner <mgatner@icloud.com>
2020-10-02 19:26:04 +0000

    Switch to asset manifests

--- src/Views/Dropzone/config.php
+++ src/Views/Dropzone/config.php
@@ -1,7 +1,4 @@
-	<!-- DropzoneJS CSS -->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.css" integrity="sha256-e47xOkXs1JXFbjjpoRr1/LhVcqSzRmGmPqsrUQeVs+g=" crossorigin="anonymous" />
-	<!-- DropzoneJS JavaScript -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.js" integrity="sha256-cs4thShDfjkqFGk5s2Lxj35sgSRr4MRcyccmi0WKqCM=" crossorigin="anonymous"></script>
+
 	<script>
 <?php
 	// Determine upload limit from PHP settings
