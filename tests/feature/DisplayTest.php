0667461504e6c3b0f4f9ab22b6eafa9972ae6c6c
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-10-09 13:59:04 +0700

    testSorts

--- tests/feature/DisplayTest.php
+++ tests/feature/DisplayTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Config\Services;
 use Myth\Auth\Test\Fakers\UserFaker;
 use Tatter\Files\Controllers\Files;
 use Tatter\Files\Exceptions\FilesException;
@@ -19,10 +20,35 @@ class DisplayTest extends FeatureTestCase
 	public function testDefaultDisplaysCards()
 	{
 		$file = fake(FileFaker::class);
-		
+
 		$result = $this->get('files');
 
 		$result->assertStatus(200);
 		$result->assertSee($file->filename);
+	}
+
+	public function provideSort()
+	{
+		yield ['filename', 'filename'];
+		yield ['localname', 'localname'];
+		yield ['clientname', 'clientname'];
+		yield ['type', 'type'];
+		yield ['size', 'size'];
+		yield ['thumbnail', 'thumbnail'];
+		yield ['invalidsort', 'filename'];
+	}
+
+	/**
+	 * @dataProvider provideSort
+	 */
+	public function testSorts(string $sort, string $configSort)
+	{
+		$_REQUEST['sort'] = $sort;
+
+		$file = fake(FileFaker::class);
+		$result = $this->get('files');
+
+		$result->assertStatus(200);
+		$this->assertEquals($configSort, service('settings')->filesSort);
 	}
 }
