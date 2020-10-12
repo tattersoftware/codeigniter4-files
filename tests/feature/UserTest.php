54d2eb8c4a3bf68fb6f80f142d01a97a4af71876
Abdul Malik Ikhsan <samsonasik@gmail.com>
2020-10-12 19:03:27 +0700

    fix UserTest::testShowsOwnFiles() with model(FileModel)->addToUser($file->id, $user->id)

---
+++ tests/feature/UserTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Tests\Support\FeatureTestCase;
+use Tests\Support\Fakers\FileFaker;
+
+class UserTest extends FeatureTestCase
+{
+	public function testShowsOwnFiles()
+	{
+		$file = fake(FileFaker::class);
+		$user = $this->login();
+
+		model('FileModel')->addToUser($file->id, $user->id);
+
+		$result = $this->get('files/user/' . $user->id);
+		$result->assertSee($file->filename);
+	}
+}
