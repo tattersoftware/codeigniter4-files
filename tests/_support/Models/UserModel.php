203fe6bd00d4acdabc833a461232cc6d7b85a163
MGatner <mgatner@icloud.com>
2020-10-15 14:33:28 +0000

    Update permissions, add logged in example

---
+++ tests/_support/Models/UserModel.php
@@ -0,0 +1,24 @@
+<?php namespace Tests\Support\Models;
+
+use Myth\Auth\Models\UserModel as MythModel;
+use Tatter\Permits\Interfaces\PermitsUserModelInterface;
+
+/**
+ * An extension of Myth's UserModel that is
+ * compatible with Tatter\Permits.
+ */
+class UserModel extends MythModel implements PermitsUserModelInterface
+{
+	/**
+	 * Returns an empty array since groups are
+	 * not currently implemented.
+	 *
+	 * @param mixed $userId = null
+	 *
+	 * @return array
+	 */
+	public function groups($userId = null): array
+	{
+		return [];
+	}
+}
